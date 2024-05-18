6807ccdeb42aec7e6d82250f6e4384e26c72c97f
Oriol Miranda <oriolmiranda15@gmail.com>
2024-05-18 21:31:24 +0200

    add topo switch

---
+++ macTopology.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Node, OVSKernelSwitch, RemoteController, OVSSwitch, OVSController
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+
+
+class LinuxRouter( Node ):
+    "A Node with IP forwarding enabled."
+
+    # pylint: disable=arguments-differ
+    def config( self, **params ):
+        super( LinuxRouter, self).config( **params )
+        # Enable forwarding on the router
+        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+
+    def terminate( self ):
+        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+        super( LinuxRouter, self ).terminate()
+
+
+class NetworkTopo( Topo ):
+    "A LinuxRouter connecting three IP subnets"
+
+    # pylint: disable=arguments-differ
+    def build( self, **_opts ):
+
+        defaultIP = '192.168.1.1/24'  # IP address for r0-eth1
+        router = self.addNode( 'r0', cls=LinuxRouter, ip=defaultIP, mac='00:00:00:00:00:01')
+
+        s1, s2 = [ self.addSwitch( s ) for s in ( 's1', 's2') ]
+
+        self.addLink( s1, router, intfName2='r0-eth1',
+                      params2={ 'ip' : defaultIP } )  # for clarity
+        self.addLink( s2, router, intfName2='r0-eth2',
+                      params2={ 'ip' : '10.0.0.1/24' } )
+        # self.addLink( s3, router, intfName2='r0-eth3',
+        #              params2={ 'ip' : '10.0.0.1/8' } )
+
+        dbserver = self.addHost( 'dbserver', ip='192.168.1.100/24',
+                           defaultRoute='via 192.168.1.1',
+                           mac='00:00:00:00:01:01' )
+        server1 = self.addHost( 'server1', ip='10.0.0.100/24',
+                           defaultRoute='via 10.0.0.1',
+                            mac='00:00:00:00:02:01' )
+        server2 = self.addHost( 'server2', ip='10.0.0.200/24',
+                           defaultRoute='via 10.0.0.1',
+                            mac='00:00:00:00:02:02' )
+        host1 = self.addHost( 'host1', ip='203.0.113.100/24',
+                          defaultRoute='via 203.0.113.1',
+                            mac='00:00:00:00:03:01' )
+
+        for h, s in [ (dbserver, s1), (server1, s2), (server2, s2)]:
+            self.addLink( h, s )
+
+        self.addLink(host1, router, intfName2='r0-eth3', params2={'ip': '203.0.113.1/24'})
+
+def run():
+    "Test linux router"
+    topo = NetworkTopo()
+    net = Mininet( topo=topo,
+    		       controller=RemoteController('c0', protocols="OpenFLow13"),
+                   waitConnected=True,
+                   switch=OVSKernelSwitch,
+                   )
+    # net = Mininet( topo=topo,
+    #               waitConnected=True )  # controller is used by s1-s3
+    net.start()
+    info( '*** Routing Table on Router:\n' )
+    info( net[ 'r0' ].cmd( 'route' ) )
+    CLI( net )
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    run()
